c5d38bb18a7771df4246e1e76cb2708a0040c16e
br-monteiro <edson.monteiro@chaordicsystems.com>
2017-12-06 20:11:27 -0200

    Adding test for type of result returded

--- 00 - PHP-FizzBuzz/tests/FizzBuzzTest.php
+++ 00 - PHP-FizzBuzz/tests/FizzBuzzTest.php
@@ -1,20 +1,25 @@
 <?php
-
 require_once 'FizzBuzz.php';
 
 use PHPUnit\Framework\TestCase as PHPUnit;
 
 class FizzBuzzTest extends PHPUnit
 {
+
     protected $fizzBuzz;
-    
+
     public function setUp()
     {
         $this->fizzBuzz = new FizzBuzz(15);
     }
-    
+
     public function testQuantityResultReturns()
     {
         $this->assertEquals(15, count($this->fizzBuzz->getResults()));
+    }
+
+    public function testReturnsArrayFromResults()
+    {
+        $this->assertEquals('array', gettype($this->fizzBuzz->getResults()), "Should be returned a array type");
     }
 }
