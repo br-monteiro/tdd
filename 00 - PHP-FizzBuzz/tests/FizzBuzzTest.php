ecd1a85abec19f492da47580bf684f3aea72459a
br-monteiro <edson.monteiro@chaordicsystems.com>
2017-12-12 13:49:01 -0200

    Adding tests for results as Object and JSON

--- 00 - PHP-FizzBuzz/tests/FizzBuzzTest.php
+++ 00 - PHP-FizzBuzz/tests/FizzBuzzTest.php
@@ -50,4 +50,35 @@ class FizzBuzzTest extends PHPUnit
     {
         $this->fizzBuzz->setQuantity(1);
     }
+
+    public function testResultAsObject()
+    {
+        $expectedObject = new \stdClass();
+        $expectedObject->r1 = 1;
+        $expectedObject->r2 = 2;
+        $expectedObject->r3 = 'Fizz';
+        $expectedObject->r4 = 4;
+        $expectedObject->r5 = 'Buzz';
+        $expectedObject->r6 = 'Fizz';
+        $expectedObject->r7 = 7;
+        $expectedObject->r8 = 8;
+        $expectedObject->r9 = 'Fizz';
+        $expectedObject->r10 = 'Buzz';
+        $expectedObject->r11 = 11;
+        $expectedObject->r12 = 'Fizz';
+        $expectedObject->r13 = 13;
+        $expectedObject->r14 = 14;
+        $expectedObject->r15 = 'FizzBuzz';
+
+        $this->assertEquals('object', gettype($this->fizzBuzz->getResultsAsObject()), "Should be returned stdClass Object");
+        $this->assertEquals($expectedObject, $this->fizzBuzz->getResultsAsObject(), "Should be returned an equal Object");
+    }
+
+    public function testResultAsJson()
+    {
+        $expectedJson = '{"r1":1,"r2":2,"r3":"Fizz","r4":4,"r5":"Buzz","r6":"Fizz","r7":7,"r8":8,"r9":"Fizz","r10":"Buzz","r11":11,"r12":"Fizz","r13":13,"r14":14,"r15":"FizzBuzz"}';
+
+        $this->assertEquals('string', gettype($this->fizzBuzz->getResultsAsJson()), "Should be returned JSON string");
+        $this->assertEquals($expectedJson, $this->fizzBuzz->getResultsAsJson(), "Should be returned JSON string");
+    }
 }
