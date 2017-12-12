925953523ea5bef1f5c8837277e3afa0bc801a4d
br-monteiro <edson.monteiro@chaordicsystems.com>
2017-12-12 20:47:47 -0200

    Reset results before executing loop

--- 00 - PHP-FizzBuzz/FizzBuzz.php
+++ 00 - PHP-FizzBuzz/FizzBuzz.php
@@ -14,6 +14,7 @@ class FizzBuzz
 
     public function run(): FizzBuzz
     {
+        $this->results = [];
         for ($i = 1; $i <= $this->quantity; $i++) {
             $fizz = $this->isFizz($i);
             $buzz = $this->isBuzz($i);
