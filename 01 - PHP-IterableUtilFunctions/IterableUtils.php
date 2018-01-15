0d96f92a95e2dffe0381ab66d88abe199ef84456
br-monteiro <edson.monteiro@chaordicsystems.com>
2018-01-14 22:25:37 -0200

    Adding Lib to arrays

--- 01 - PHP-IterableUtilFunctions/IterableUtils.php
+++ 01 - PHP-IterableUtilFunctions/IterableUtils.php
@@ -1,3 +1,41 @@
 <?php
+namespace App\Utils;
 
-class IterableUtils {}
+class IterableUtils
+{
+
+    public static function map(array $arr, callable $callback): array
+    {
+        $arrResult = [];
+
+        foreach ($arr as $index => $value) {
+            $arrResult[] = $callback($value, $index);
+        }
+
+        return $arrResult;
+    }
+
+    public static function find(array $arr, callable $callback)
+    {
+        foreach ($arr as $index => $value) {
+            if ($callback($value, $index) === true) {
+                return $value;
+            }
+        }
+
+        return null;
+    }
+
+    public static function filter(array $arr, callable $callback): array
+    {
+        $arrResult = [];
+
+        foreach ($arr as $index => $value) {
+            if ($callback($value, $index) === true) {
+                $arrResult[] = $value;
+            }
+        }
+
+        return $arrResult;
+    }
+}
